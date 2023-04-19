69f426a3ba602cc2b699da91ecdf762f6a9b4110
Flavia Costa <flavinha.goncalves@gmail.com>
2023-04-18 23:16:34 -0300

    db.json rota

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
-const http = require('http');
-const PORT = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
-});
-
-const jsonServer = require("json-server");
-const cors = require("cors");
-const path = require("path");
+const jsonServer = require('json-server');
+const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, "db", "db.json"));
 const middlewares = jsonServer.defaults();
 
-server.use(cors());
-server.use("/api", router);
+server.use(middlewares);
+server.use(router);
 
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
