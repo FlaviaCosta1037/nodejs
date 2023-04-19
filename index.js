78d5ad18c417f12cc8ebd9b3ef29e0304826332d
Flavia Costa <flavinha.goncalves@gmail.com>
2023-04-18 23:25:46 -0300

    Teste para leitura db.json

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, "db", "db.json"));
-const middlewares = jsonServer.defaults();
+const http = require('http');
+const fs = require('fs');
+const PORT = 3000;
 
-server.use(middlewares);
-server.use(router);
+const server = http.createServer((req, res) => {
+  if (req.url === '/api/data') { // Define a rota para a API
+    fs.readFile('./db/db.json', (err, data) => { // Lê o arquivo db.json
+      if (err) throw err;
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.end(data); // Envia o conteúdo como resposta da requisição
+    });
+  } else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Not found');
+  }
+});
 
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
