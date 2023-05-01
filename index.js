707ffa4d2b29453d3277f647a2103a3046df01ee
Flavia Costa <flavinha.goncalves@gmail.com>
2023-05-01 14:35:51 -0300

    Add um metodo post como teste na api

--- index.js
+++ index.js
@@ -3,12 +3,25 @@ const fs = require('fs');
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-  if (req.url === '/user') { // Define a rota para a API
+  if (req.method ==='GET' && req.url === '/user') { // Define a rota para a API
     fs.readFile('./db/db.json', (err, data) => { // Lê o arquivo db.json
       if (err) throw err;
       res.statusCode = 200;
       res.setHeader('Content-Type', 'application/json');
       res.end(data); // Envia o conteúdo como resposta da requisição
+    });
+  }else if(req.method ==='POST' && req.url ==='/user'){
+    let body = '';
+    req.on('data', chunk => {
+      body += chunk.toString();
+    });
+    req.on('end', () => {
+      fs.writeFile('./db/db.json', body, err => { // Grava o corpo da requisição no arquivo db.json
+        if (err) throw err;
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Data saved successfully');
+      });
     });
   } else {
     res.statusCode = 404;
